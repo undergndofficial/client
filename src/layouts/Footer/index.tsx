c6c52edd1227b494e96a8f59bf856191b77e1d1b
Huiyeong <53117014+klloo@users.noreply.github.com>
2023-09-27 14:52:23 +0900

    style: footer 스타일 수정

--- src/layouts/Footer/index.tsx
+++ src/layouts/Footer/index.tsx
@@ -1,33 +1,57 @@
 import React from 'react';
-import { Container, ButtonWrapper, InfoWrapper, FlexWrapper } from './style';
+import {
+  Container,
+  ButtonWrapper,
+  InfoWrapper,
+  FlexWrapper,
+  ButtonListDiv,
+  SnsIconWrapper,
+} from './style';
+import {
+  IoLogoYoutube,
+  IoLogoInstagram,
+  IoLogoTwitter,
+  IoLogoFacebook,
+} from 'react-icons/io5';
 
 /**
  * 레이아웃 Footer
  */
 function Footer() {
+  const ICON_SIZE = 21;
   return (
     <Container>
+      <SnsIconWrapper>
+        <IoLogoYoutube size={ICON_SIZE} />
+        <IoLogoInstagram size={ICON_SIZE} />
+        <IoLogoTwitter size={ICON_SIZE} />
+        <IoLogoFacebook size={ICON_SIZE} />
+      </SnsIconWrapper>
       <ButtonWrapper>
-        <div>고객센터</div>
-        <div>후원</div>
-        <div>개인정보</div>
-        <div>법적고지</div>
-        <div>about us</div>
-        <div>문의하기</div>
-        <div>대학 한눈에보기</div>
+        <ButtonListDiv>
+          <div>고객센터</div>
+          <div>후원</div>
+          <div>개인정보</div>
+          <div>법적고지</div>
+        </ButtonListDiv>
+        <ButtonListDiv>
+          <div>about us</div>
+          <div>문의하기</div>
+          <div>대학 한눈에보기</div>
+        </ButtonListDiv>
       </ButtonWrapper>
       <InfoWrapper>
         <FlexWrapper>
           <div>언더그라운드</div>
-          <div> 통신판매법 신고번호 : 123456</div>
-          <div> 전화번호:01062740069</div>
+          <div> 통신판매법 신고번호: 제 xXX-서울 종로-xxxx호</div>
+          <div> 전화번호: xxx-xxxx-xxxx</div>
         </FlexWrapper>
         <div>대표: 문지욱</div>
-        <div>이메일주소: sallormoon917@naver.com</div>
+        <div>이메일주소: undergndofficial@gmaill.com</div>
         <div>주소: 충북 제천시 한수면 봉화재길 517</div>
-        <div>사업자 등록 번호: 123456</div>
+        <div>사업자 등록 번호: xxx-xx-xxxxx</div>
         <div>클라우드 호스팅: Amazon Web Sevices Inc.</div>
-        <div>공정거래위원회 웹사이트: 12356789</div>
+        <div>공정거래위원회 웹사이트: https://www.ftc.go.kr/</div>
       </InfoWrapper>
     </Container>
   );
--- src/layouts/Footer/style.tsx
+++ src/layouts/Footer/style.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import theme from 'styles/theme';
 
 export const Container = styled.footer`
   display: flex;
@@ -11,23 +12,45 @@ export const Container = styled.footer`
 `;
 
 export const ButtonWrapper = styled.div`
-  cursor: pointer;
   display: flex;
-  width: 35%;
-  row-gap: 1.2rem;
-  column-gap: 4rem;
   align-items: center;
   justify-content: center;
   flex-wrap: wrap;
+  flex-direction: column;
+  gap: 1.5rem;
+  font-size: 0.75rem;
+  @media ${theme.device.phone} {
+    gap: 1rem;
+  }
+`;
+
+export const ButtonListDiv = styled.div`
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  row-gap: 1rem;
   & div {
     display: flex;
     align-items: center;
     gap: 4px;
+    padding: 0 3rem;
+    border-right: 1px solid white;
+    @media ${theme.device.phone} {
+      border-right: none;
+    }
+  }
+  & :last-child {
+    border-right: none;
+  }
+  @media ${theme.device.phone} {
+    flex-direction: column;
   }
 `;
 
 export const InfoWrapper = styled.div`
-  font-size: 0.8rem;
+  font-size: 0.75rem;
   display: flex;
   flex-direction: column;
   gap: 1rem;
@@ -37,8 +60,19 @@ export const InfoWrapper = styled.div`
 
 export const FlexWrapper = styled.div`
   display: flex;
-  gap: 1.5rem;
+  column-gap: 1.5rem;
+  row-gap: 1rem;
   flex-wrap: wrap;
   align-items: center;
   justify-content: center;
+  @media ${theme.device.phone} {
+    flex-direction: column;
+  }
+`;
+
+export const SnsIconWrapper = styled.div`
+  display: flex;
+  gap: 1.5rem;
+  color: var(--color-font);
+  cursor: pointer;
 `;
